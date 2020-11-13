3d6cc5c7e9a5ac09023e018a6480ac2cec6ca8bd
Malik Ardhiansyah <ardhiansyahmalik1200@gmail.com>
2020-11-13 23:49:30 +0700

    Fix caching in workbox

--- service-worker.js
+++ service-worker.js
@@ -127,50 +127,6 @@ workbox.routing.registerRoute(
     })
 );
 
-// self.addEventListener("install", function(event) {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME).then((cache) => {
-//             return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-
-// self.addEventListener("fetch", function(event) {
-//     const base_url1 = "https://api.football-data.org/v2/";
-//     const base_url2 = "https://crests.football-data.org";
-//     if (event.request.url.indexOf(base_url1) > -1 || event.request.url.indexOf(base_url2) > -1) {
-//         event.respondWith(
-//             caches.open(CACHE_NAME).then((cache) => {
-//                 return fetch(event.request).then((response) => {
-//                     cache.put(event.request.url, response.clone());
-//                     return response;
-//                 })
-//             })
-//         );
-//     } else {
-//         event.respondWith(
-//             caches.match(event.request, { ignoreSearch: true }).then((response) => {
-//                 return response || fetch(event.request);
-//             })
-//         )
-//     }
-// });
-
-// self.addEventListener("activate", function(event) {
-//     event.waitUntil(
-//         caches.keys().then((cacheNames) => {
-//             return Promise.all(
-//                 cacheNames.map((cacheName) => {
-//                     if (cacheName != CACHE_NAME) {
-//                         console.log("ServiceWorker: cache " + cacheName + " dihapus");
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
-
 self.addEventListener("push", function(event) {
     let body;
     if (event.data) {
